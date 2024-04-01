33e0bfc5b2c3556f91d0bdf6808e681cdd9e0365
chami98 <chamikara98@gmail.com>
2024-04-01 15:14:29 +0530

    Add routes for signin and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import SignInSide from './pages/SignInSide'
 import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-      <SignInSide />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Dashboard />}
+          />
+          <Route
+            path="/signin"
+            element={<SignInSide />}
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
