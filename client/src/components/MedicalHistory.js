cb776ef706ed65bed52438cceb09b8e59a9dc594
chami98 <chamikara98@gmail.com>
2024-04-20 20:29:47 +0530

    Recreate Medical History Component

--- client/src/components/MedicalHistory.js
+++ client/src/components/MedicalHistory.js
@@ -1,32 +1,45 @@
 import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 
 export default function MedicalHistory() {
     const [formData, setFormData] = useState({
-        age: '',
         gender: '',
-        allergies: false,
-        diabetes: false,
-        hypertension: false,
-        heartDisease: false,
-        otherConditions: ''
+        age: '',
+        allergies: [],
+        medications: []
     });
 
     const handleChange = (event) => {
         const { name, value, type, checked } = event.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: type === 'checkbox' ? checked : value
-        }));
+        if (type === 'checkbox') {
+            if (checked) {
+                setFormData(prevState => ({
+                    ...prevState,
+                    [name]: [...formData[name], value]
+                }));
+            } else {
+                setFormData(prevState => ({
+                    ...prevState,
+                    [name]: formData[name].filter(item => item !== value)
+                }));
+            }
+        } else {
+            setFormData(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        }
     };
 
     const handleSubmit = (event) => {
@@ -46,20 +59,23 @@ export default function MedicalHistory() {
                 <form onSubmit={handleSubmit}>
                     <Grid container spacing={3} justifyContent="center">
                         <Grid item xs={12} md={6}>
-                            <Box display="flex" sx={{ alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-                                <RadioGroup
-                                    name="gender"
+                            <Typography variant="h6" component="h3" color="textPrimary" gutterBottom>
+                                Gender & Age
+                            </Typography>
+                            <FormControl fullWidth margin="normal">
+                                <InputLabel id="gender-label">Gender</InputLabel>
+                                <Select
+                                    labelId="gender-label"
+                                    id="gender-select"
                                     value={formData.gender}
                                     onChange={handleChange}
-                                    row
+                                    name="gender"
                                 >
-                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                                </RadioGroup>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
+                                    <MenuItem value="male">Male</MenuItem>
+                                    <MenuItem value="female">Female</MenuItem>
+                                    <MenuItem value="other">Other</MenuItem>
+                                </Select>
+                            </FormControl>
                             <TextField
                                 label="Age"
                                 name="age"
@@ -72,31 +88,42 @@ export default function MedicalHistory() {
                             />
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <Box display="flex" sx={{ alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-                                <FormControlLabel
-                                    control={<Checkbox checked={formData.diabetes} onChange={handleChange} name="diabetes" />}
-                                    label="Diabetes"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox checked={formData.hypertension} onChange={handleChange} name="hypertension" />}
-                                    label="Hypertension"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox checked={formData.heartDisease} onChange={handleChange} name="heartDisease" />}
-                                    label="Heart Disease"
-                                />
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                label="Other Medical Conditions"
-                                name="otherConditions"
-                                value={formData.otherConditions}
-                                onChange={handleChange}
-                                fullWidth
-                                margin="normal"
-                                variant="outlined"
-                            />
+                            <Typography variant="h6" component="h3" color="textPrimary" gutterBottom>
+                                Allergies and Medications
+                            </Typography>
+                            <FormControl fullWidth margin="normal">
+                                <InputLabel id="allergies-label">Do you have any known allergies?</InputLabel>
+                                <Select
+                                    labelId="allergies-label"
+                                    id="allergies-select"
+                                    multiple
+                                    value={formData.allergies}
+                                    onChange={handleChange}
+                                    name="allergies"
+                                >
+                                    <MenuItem value="Medications">Medications</MenuItem>
+                                    <MenuItem value="Foods">Foods</MenuItem>
+                                    <MenuItem value="Environmental factors">Environmental factors</MenuItem>
+                                    <MenuItem value="Insects">Insects</MenuItem>
+                                    <MenuItem value="Latex">Latex</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl fullWidth margin="normal">
+                                <InputLabel id="medications-label">Are you currently taking any medications?</InputLabel>
+                                <Select
+                                    labelId="medications-label"
+                                    id="medications-select"
+                                    multiple
+                                    value={formData.medications}
+                                    onChange={handleChange}
+                                    name="medications"
+                                >
+                                    <MenuItem value="Prescription drugs">Prescription drugs</MenuItem>
+                                    <MenuItem value="Over-the-counter medications">Over-the-counter medications</MenuItem>
+                                    <MenuItem value="Supplements">Supplements</MenuItem>
+                                    <MenuItem value="Herbal remedies">Herbal remedies</MenuItem>
+                                </Select>
+                            </FormControl>
                         </Grid>
                     </Grid>
                 </form>
