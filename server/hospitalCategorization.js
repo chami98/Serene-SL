1600a7c2ddfcdd944c03baf23614ba4fdf03a04c
chami98 <chamikara98@gmail.com>
2024-04-25 22:23:05 +0530

    Add Phone numbers

--- server/hospitalCategorization.js
+++ server/hospitalCategorization.js
@@ -7,7 +7,8 @@ const hospitalCategorization = {
                 "location": "Colombo 10,",
                 "description": "The largest teaching hospital in and the final referral centre in the country consisting of 3000 beds.",
                 "url": "https://www.nhsl.health.gov.lk/",
-                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg"
+                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg",
+                "phoneNumber": "+94 11 269 1111"
 
             },
             {
@@ -16,7 +17,8 @@ const hospitalCategorization = {
                 "location": "218, Cotta Road, Borella,",
                 "description": "An ISO 9001:2015 QMS certified hospital with over 30 years of experience in. Renowned for having the longest standing Kidney Transplantation and Hemodialysis Programme in.",
                 "url": "https://www.westernhospital.lk/",
-                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPZmao2OrtSvuGQhDcS126Sk2DCc96ya2yzTbDvbHRZA&s"
+                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPZmao2OrtSvuGQhDcS126Sk2DCc96ya2yzTbDvbHRZA&s",
+                "phoneNumber": "+94 11 7 699 000"
 
             }
         ],
@@ -26,7 +28,8 @@ const hospitalCategorization = {
                 "location": "Colombo,",
                 "description": "Part of Asiri Health, this centre is dedicated to treating chronic diseases like heart conditions.",
                 "url": "https://www.asirihealth.com/",
-                "imageUrl": "https://cbr.lk/wp-content/uploads/2016/06/Asiri-Health.jpg"
+                "imageUrl": "https://cbr.lk/wp-content/uploads/2016/06/Asiri-Health.jpg",
+                "phoneNumber": "+94 11 4 414 414"
 
 
             },
@@ -35,7 +38,8 @@ const hospitalCategorization = {
                 "location": "43 Mahabage Road, Ragama",
                 "description": "A fully-fledged multi-specialty general healthcare facility equipped with state-of-the-art medical diagnostic technology as well as advanced medical and surgical therapeutic technology.",
                 "url": "https://www.melstahospitalsragama.lk/",
-                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLKN2Uf_SIgPtxH424Zy9T6IlOIDPk7pwYfq51wPay_A&s"
+                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLKN2Uf_SIgPtxH424Zy9T6IlOIDPk7pwYfq51wPay_A&s",
+                "phoneNumber": "+94 11 2 963 963"
 
             }
         ],
@@ -46,7 +50,8 @@ const hospitalCategorization = {
                 "location": "Colombo 10,",
                 "description": "The largest teaching hospital in and the final referral centre in the country consisting of 3000 beds.",
                 "url": "https://www.nhsl.health.gov.lk/",
-                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg"
+                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg",
+                "phoneNumber": "+94 11 269 1111"
 
             }
         ],
@@ -56,8 +61,8 @@ const hospitalCategorization = {
                 "location": "Colombo,",
                 "description": "Part of Asiri Health, this centre is dedicated to treating neurological disorders.",
                 "url": "https://www.asirihealth.com/",
-                "imageUrl": "https://cbr.lk/wp-content/uploads/2016/06/Asiri-Health.jpg"
-
+                "imageUrl": "https://cbr.lk/wp-content/uploads/2016/06/Asiri-Health.jpg",
+                "phoneNumber": "+94 11 466 5500"
             },
             {
                 "name": "National Hospital of Sri Lanka",
@@ -65,7 +70,8 @@ const hospitalCategorization = {
                 "location": "Colombo 10,",
                 "description": "The largest teaching hospital in and the final referral centre in the country consisting of 3000 beds.",
                 "url": "https://www.nhsl.health.gov.lk/",
-                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg"
+                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg",
+                "phoneNumber": "+94 11 269 1111"
 
             },
             {
@@ -74,7 +80,9 @@ const hospitalCategorization = {
                 "location": "3 Alfred Place, Colombo 03",
                 "description": "A multi-speciality private hospital that treats patients visiting from around the world, in Colombo,.",
                 "url": "https://www.durdans.com/",
-                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg"
+                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg",
+                "phoneNumber": "+94 11 214 0000"
+
 
             }
         ],
@@ -85,7 +93,8 @@ const hospitalCategorization = {
                 "location": "3 Alfred Place, Colombo 03",
                 "description": "A multi-speciality private hospital that treats patients visiting from around the world, in Colombo,.",
                 "url": "https://www.durdans.com/",
-                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg"
+                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg",
+                "phoneNumber": "+94 11 214 0000"
             },
             {
                 "name": "National Hospital of Sri Lanka",
@@ -93,7 +102,9 @@ const hospitalCategorization = {
                 "location": "Colombo 10,",
                 "description": "The largest teaching hospital in and the final referral centre in the country consisting of 3000 beds.",
                 "url": "https://www.nhsl.health.gov.lk/",
-                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg"
+                "imageUrl": "https://island.lk/wp-content/uploads/2021/09/genaral-hospital.jpg",
+                "phoneNumber": "+94 11 269 1111"
+
 
             }
         ],
@@ -103,8 +114,8 @@ const hospitalCategorization = {
                 "location": "Colombo,",
                 "description": "Part of Asiri Health, this centre is dedicated to treating cardiovascular conditions.",
                 "url": "https://www.asirihealth.com/",
-                "imageUrl": "https://cbr.lk/wp-content/uploads/2016/06/Asiri-Health.jpg"
-
+                "imageUrl": "https://cbr.lk/wp-content/uploads/2016/06/Asiri-Health.jpg",
+                "phoneNumber": "+94 11 452 4400"
             },
             {
                 "name": "Durdans Hospital",
@@ -112,7 +123,9 @@ const hospitalCategorization = {
                 "location": "3 Alfred Place, Colombo 03,",
                 "description": "A multi-speciality private hospital that treats patients visiting from around the world, in Colombo,.",
                 "url": "https://www.durdans.com/",
-                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg"
+                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg",
+                "phoneNumber": "+94 11 214 0000"
+
             }
         ],
         "DermatologicalIssue": [
@@ -122,7 +135,9 @@ const hospitalCategorization = {
                 "location": "3 Alfred Place, Colombo 03,",
                 "description": "A multi-speciality private hospital that treats patients visiting from around the world, in Colombo,.",
                 "url": "https://www.durdans.com/",
-                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg"
+                "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/57/170120_Durdans_Hospital_Logo.jpg",
+                "phoneNumber": "+94 11 214 0000"
+
             },
             {
                 "name": "St Joseph Private Hospital",
@@ -130,8 +145,8 @@ const hospitalCategorization = {
                 "location": "Harding Avenue, Malpas, Newport, NP20 6ZE",
                 "description": "A private hospital providing a range of services including dermatology.",
                 "url": "https://sjhospital.lk/",
-                "imageUrl": "https://sjhospital.lk/wp-content/uploads/2023/12/St-Jospeh-Hospital-Entrance-1024x399.png"
-
+                "imageUrl": "https://sjhospital.lk/wp-content/uploads/2023/12/St-Jospeh-Hospital-Entrance-1024x399.png",
+                "phoneNumber": "+94 11 784 8484"
             }
         ],
         "MentalHealthConcern": [
@@ -140,7 +155,9 @@ const hospitalCategorization = {
                 "location": "B231, Mulleriyawa,",
                 "description": "The largest tertiary care hospital in caring for patients with mental illness.",
                 "url": "http://www.nimh.health.gov.lk/",
-                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK7UzMd3miz0uQ3beTIOhucHABhnzU1RnEHAhXtwv5yA&s"
+                "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK7UzMd3miz0uQ3beTIOhucHABhnzU1RnEHAhXtwv5yA&s",
+                "phoneNumber": "+94 11 257 8234"
+
 
             }
         ]
@@ -151,26 +168,25 @@ const hospitalCategorization = {
             "name": "Sen Wellness Sanctuary",
             "location": "Rekawa, Tangalle, Sri Lanka",
             "url": "https://www.senwellnesssanctuary.com/",
-            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIxvh7zwY3F4DAMv51mxHY-kFbtCAGTjgRieIyMPcgWA&s"
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIxvh7zwY3F4DAMv51mxHY-kFbtCAGTjgRieIyMPcgWA&s",
+            "phoneNumber": "+94 77 390 4800"
         },
 
-        {
-            "name": "Barberyn Reef Ayurveda Resort",
-            "location": "Beruwala, Sri Lanka",
-            "url": "https://barberynresorts.com/beruwala/",
-            "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/e7/b2/d5/barberyn-reef-ayurveda.jpg?w=700&h=-1&s=1"
-        },
         {
             "name": "Santani Wellness - Kandy",
             "location": "Kandy, Sri Lanka",
             "url": "https://www.santani.lk/",
-            "imageUrl": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/165748971.jpg?k=484ddceb3813a5c0e3e20257b97ce6557cbd5d23f64d1619984f5cd062e90382&o=&hp=1"
+            "imageUrl": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/165748971.jpg?k=484ddceb3813a5c0e3e20257b97ce6557cbd5d23f64d1619984f5cd062e90382&o=&hp=1",
+            "phoneNumber": "+94 76 399 1919"
+
         },
         {
             "name": "98 Acres Resort and Spa",
             "location": "Ella, Sri Lanka",
             "url": "https://www.resort98acres.com/",
-            "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/77/7a/90/98-acres-resort-and-spa.jpg?w=1200&h=-1&s=1"
+            "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/77/7a/90/98-acres-resort-and-spa.jpg?w=1200&h=-1&s=1",
+            "phoneNumber": "+94 57 205 0050"
+
         }
     ],
     "ayurveda_hospitals": [
@@ -178,19 +194,23 @@ const hospitalCategorization = {
             "name": "Ayuthantra Ayurveda & Wellness",
             "location": "No. 44, Medagama Road, Kandy",
             "url": "https://www.ayuthantra.com/",
-            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ848AnFA13R5Yci7ZXtY62iMBNm_etgu2bDvibjBrY9w&s"
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ848AnFA13R5Yci7ZXtY62iMBNm_etgu2bDvibjBrY9w&s",
+            "phoneNumber": "+94 77 666 5521"
         },
         {
             "name": "Siddhalepa Ayurveda Hospital",
             "location": "Siddhalepa, Colombo,",
             "url": "https://www.siddhalepa.com/",
-            "imageUrl": "https://d3bfon2twrfdyk.cloudfront.net/2018/10/SAH-800-x-450.jpg"
+            "imageUrl": "https://d3bfon2twrfdyk.cloudfront.net/2018/10/SAH-800-x-450.jpg",
+            "phoneNumber": "+94 11 273 8622"
+
         },
         {
             "name": "Winsetha Ayurveda Hospital",
             "location": "NO.5, BARNES AVENUE, HOTEL ROAD, MT LAVINIA",
             "url": "https://winsethaayurveda.com/",
-            "imageUrl": "https://ayurvediccentresin.com/wp-content/uploads/2017/06/Winsetha-Ayurveda-Hospital-in-Lavinia.jpg"
+            "imageUrl": "https://ayurvediccentresin.com/wp-content/uploads/2017/06/Winsetha-Ayurveda-Hospital-in-Lavinia.jpg",
+            "phoneNumber": "+94 77 555 6466"
         },
     ]
 
