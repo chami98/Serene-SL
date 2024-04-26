c7b593c72fe8c76dead7c37c7c53b635f67037e7
chami98 <chamikara98@gmail.com>
2024-04-26 12:09:49 +0530

    Add Icon

--- client/src/components/RecommendedHospital.js
+++ client/src/components/RecommendedHospital.js
@@ -4,8 +4,9 @@ import hospitalData from '../utils/HospitalData';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
-export default function RecommendedHospital({ recomendedHospitals, recommendedWellnessCenter, recommendedAyurvedaHospital }) {
+export default function RecommendedHospital({ recomendedHospitals, recommendedWellnessCenter, recommendedAyurvedaHospital, setActiveStep }) {
 
     const [hospitals, setHospitals] = useState([]);
     const [wellnessCenter, setWellnessCenter] = useState([]);
@@ -22,6 +23,10 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
     };
 
 
+    const handleHospital = () => {
+        setActiveStep(9);
+        console.log('clicked')
+    }
 
     const handleHover = (e) => {
         e.currentTarget.style.transform = 'scale(1.05)';
@@ -32,11 +37,10 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
     };
     return (
         <>
-            <Box textAlign="center">
+            <Box mt={2} textAlign="center">
                 <Typography
                     variant="h5"
                     component="h2"
-                    color="textPrimary"
                     gutterBottom
                     sx={{ mb: '25px' }}
                 >
@@ -45,38 +49,47 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
             </Box>
             <Grid container spacing={3} justifyContent="center">
                 {hospitals.map((hospital, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={index}>
-                        <Link to={hospital.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-                            <Card
-                                style={cardStyles}
-                                onMouseEnter={handleHover}
-                                onMouseLeave={handleHoverOut}
-                            >
-                                <CardMedia
-                                    component="img"
-                                    alt={hospital.name}
-                                    height="180"
-                                    image={hospital.imageUrl}
-                                />
-                                <CardContent>
-                                    <Typography variant="h5" component="h2">
-                                        {hospital.name}
-                                    </Typography>
-                                    <Typography color="textSecondary">
-                                        {hospital.type}
-                                    </Typography>
-                                    <Typography variant="body2" component="p">
-                                        {hospital.location}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Link>
+                    <Grid item xs={12} sm={6} md={4} key={index}
+                    >
+                        <Card
+                            style={{ ...cardStyles, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
+                            onMouseEnter={handleHover}
+                            onMouseLeave={handleHoverOut}
+                            onClick={() => handleHospital()}
+
+                        >
+                            <CardMedia
+                                component="img"
+                                alt={hospital.name}
+                                height="180"
+                                image={hospital.imageUrl}
+                            />
+                            <CardContent sx={{ padding: '16px', minHeight: '120px' }}>
+                                <Typography variant="h5" component="h2" gutterBottom>
+                                    {hospital.name}
+                                </Typography>
+                                <Typography color="textSecondary">
+                                    {hospital.type}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {hospital.location}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p" display="inline">
+                                    <LocalPhoneIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
+                                    {hospital.phoneNumber}
+                                </Typography>
+
+                            </CardContent>
+
+                        </Card>
+
                     </Grid>
                 ))}
             </Grid>
+
             <Grid container spacing={3} justifyContent="center">
                 <Grid item xs={12} md={6}>
-                    <Box textAlign="center">
+                    <Box mt={2} textAlign="center">
                         <Typography
                             variant="h5"
                             component="h2"
@@ -92,7 +105,7 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                             <Grid item xs={12} sm={6} md={12} key={index}>
                                 <Link to={wCenter.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
                                     <Card
-                                        style={cardStyles}
+                                        style={{ ...cardStyles, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
                                         onMouseEnter={handleHover}
                                         onMouseLeave={handleHoverOut}
                                     >
@@ -102,8 +115,8 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                                             height="180"
                                             image={wCenter.imageUrl}
                                         />
-                                        <CardContent>
-                                            <Typography variant="h5" component="h2">
+                                        <CardContent sx={{ padding: '16px', minHeight: '120px' }}>
+                                            <Typography variant="h5" component="h2" gutterBottom>
                                                 {wCenter.name}
                                             </Typography>
                                             <Typography color="textSecondary">
@@ -111,6 +124,9 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                                             </Typography>
                                             <Typography variant="body2" component="p">
                                                 {wCenter.location}
+                                            </Typography> <Typography variant="body2" color="textSecondary" component="p" display="inline">
+                                                <LocalPhoneIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
+                                                {wCenter.phoneNumber}
                                             </Typography>
                                         </CardContent>
                                     </Card>
@@ -122,7 +138,7 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
 
                 {/* Recommended Ayurveda Hospital */}
                 <Grid item xs={12} md={6}>
-                    <Box textAlign="center">
+                    <Box mt={2} textAlign="center">
                         <Typography
                             variant="h5"
                             component="h2"
@@ -138,7 +154,7 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                             <Grid item xs={12} sm={6} md={12} key={index}>
                                 <Link to={aHospital.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
                                     <Card
-                                        style={cardStyles}
+                                        style={{ ...cardStyles, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
                                         onMouseEnter={handleHover}
                                         onMouseLeave={handleHoverOut}
                                     >
@@ -148,8 +164,8 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                                             height="180"
                                             image={aHospital.imageUrl}
                                         />
-                                        <CardContent>
-                                            <Typography variant="h5" component="h2">
+                                        <CardContent sx={{ padding: '16px', minHeight: '120px' }}>
+                                            <Typography variant="h5" component="h2" gutterBottom>
                                                 {aHospital.name}
                                             </Typography>
                                             <Typography color="textSecondary">
@@ -158,6 +174,10 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                                             <Typography variant="body2" component="p">
                                                 {aHospital.location}
                                             </Typography>
+                                            <Typography variant="body2" color="textSecondary" component="p" display="inline">
+                                                <LocalPhoneIcon style={{ verticalAlign: 'middle', marginRight: '4px' }} />
+                                                {aHospital.phoneNumber}
+                                            </Typography>
                                         </CardContent>
                                     </Card>
                                 </Link>
@@ -165,7 +185,7 @@ export default function RecommendedHospital({ recomendedHospitals, recommendedWe
                         ))}
                     </Grid>
                 </Grid>
-            </Grid>
+            </Grid >
 
         </>
 
